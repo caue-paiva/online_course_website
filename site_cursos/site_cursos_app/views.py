74ce3bd8488c230bed9ad37e2110a1e7902149b8
caue <cauepaivalira@outlook.com>
2024-12-10 17:35:27 -0300

    FEAT1: Página de Login agora redireciona para dashboard, FEAT2: Login funcionando, cria novo usuario (add no BD) ou loga ele, FEAT3: Classes de curso e de módulo criadas

--- site_cursos/site_cursos_app/views.py
+++ site_cursos/site_cursos_app/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render,redirect
+from .models import Aluno
+from django.contrib.sessions.backends.db import SessionStore
 # Create your views here.
 
 def login(request):
-   return render(request,"authentication.html")
+   if request.method == "POST": #clicou no botão
+         name = request.POST.get("name")
+         email = request.POST.get("email")
+         password = request.POST.get("password")
+
+         alunos = Aluno.objects.filter(
+            email=email,
+            senha=password
+         )
+
+         aluno: Aluno
+         if len(alunos) > 1:
+            print("-- Achou mais de um Aluno com mesmo login, teve problema ---")
+         elif len(alunos) == 0: #nenhum aluno igual
+            print("---- Criando Novo Aluno ----")
+            aluno = Aluno.objects.create(
+                nome=name,
+                senha=password,
+                email=email
+            )
+         else:
+            aluno = alunos[0]
+            print("-- Aluno já Existia ---")
+         
+         request.session["user_id"] = aluno.id #guarda os dados do login na sessão
+         request.session["user_name"] = aluno.nome
+         request.session["user_email"] = aluno.email
+         
+         return redirect("dashboard")  # Redirect to the dashboard view
+
+   return render(request, "login.html")
+
+def dashboard(request):
+   return render(request,"dashboard.html")
