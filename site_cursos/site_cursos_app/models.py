16d6faa81441be8ab3409d18a72c3756b9d0fee1
caue <cauepaivalira@outlook.com>
2024-12-10 15:16:45 -0300

    FEAT1: Template do login criada e colocada em views, FEAT2: Modelo abstrato do usuario e modelo concreto do aluno para interação com o BD colocado em models.py

--- site_cursos/site_cursos_app/models.py
+++ site_cursos/site_cursos_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from abc import ABC,abstractmethod
 
 # Create your models here.
+
+class Usuario(models.Model,ABC): #classe abstrata
+   
+
+   class Meta:
+      abstract = True #o Django pede que vc faça isso eu acho
+
+class Aluno(Usuario):
+   pass #herda de usuário
--- site_cursos/site_cursos_app/views.py
+++ site_cursos/site_cursos_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def login(request):
-   return HttpResponse("Olá Mundo")
+   return render(request,"authentication.html")
